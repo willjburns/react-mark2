779c8c44d2722a4331c67cd4925f652f9456274f
willjburns <willjburns80@gmail.com>
2021-03-05 07:36:30 -0800

    second go round

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
+import profileImage from './img/profile-image.jpg';
 import './App.css';
 
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCCtkl_rr-spMhBFgP2lxcYpZZk0RY2xOw",
+    authDomain: "cas-project1.firebaseapp.com",
+    projectId: "cas-project1",
+    storageBucket: "cas-project1.appspot.com",
+    messagingSenderId: "1059867147493",
+    appId: "1:1059867147493:web:63fc2249cba7bf9bca280c",
+    measurementId: "G-QMEZYLP0G1"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 function App() {
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> My React App</h1>
+      <h2>Author: Will Burns-ish</h2>
+      <img width="500" height="625" src={profileImage} alt="profile-image" />
+      <section>
+        <SignIn/>
+      </section>
     </div>
   );
 }
 
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+return(
+    <button onClick={signInWithGoogle}>Sign in with Google</button>
+  )
+}
+
+function SignOut() {
+  return auth.currentUser && (
+
+    <button onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
 export default App;
